30f8e0903de89550d0fbf4e4a39974dd0be9abc8
Saket-Bharade <147977902+Saket-Bharade@users.noreply.github.com>
2023-10-16 11:09:42 -0400

    cleaned up code

--- maincode.py
+++ maincode.py
@@ -59,41 +59,80 @@ rawdata = df
 print (rawdata.head())
 print (rawdata.info())
 
-rawdata.hist(bins=50, figsize=(20,15))
-save_fig("attribute_histogram_plots")
-plt.show()
+# rawdata.hist(bins=50, figsize=(20,15))
+# save_fig("attribute_histogram_plots")
+# plt.show()
 
-np.random.seed(2)
+# np.random.seed(2)
 
-def split_train_test(data, test_ratio):
-    shuffled_indices = np.random.permutation(len(data))
-    test_set_size = int(len(data) * test_ratio)
-    test_indices = shuffled_indices[:test_set_size]
-    train_indices = shuffled_indices[test_set_size:]
-    return data.iloc[train_indices], data.iloc[test_indices]
+# def split_train_test(data, test_ratio):
+#     shuffled_indices = np.random.permutation(len(data))
+#     test_set_size = int(len(data) * test_ratio)
+#     test_indices = shuffled_indices[:test_set_size]
+#     train_indices = shuffled_indices[test_set_size:]
+#     return data.iloc[train_indices], data.iloc[test_indices]
 
-train_set, test_set = split_train_test(rawdata, 0.2)
-print ("train", len(train_set))
+# train_set, test_set = split_train_test(rawdata, 0.2)
+# print ("train", len(train_set))
 
-from zlib import crc32
+# from zlib import crc32
 
-def test_set_check(identifier, test_ratio):
-    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+# def test_set_check(identifier, test_ratio):
+#     return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
 
-def split_train_test_by_id(data, test_ratio, id_column):
-    ids = data[id_column]
-    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
-    return data.loc[~in_test_set], data.loc[in_test_set]
+# def split_train_test_by_id(data, test_ratio, id_column):
+#     ids = data[id_column]
+#     in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+#     return data.loc[~in_test_set], data.loc[in_test_set]
 
-print ("test", test_set.head)
+# print ("test", test_set.head)
 
+# from sklearn.model_selection import train_test_split
+
+# train_set, test_set = train_test_split(rawdata, test_size=0.2, random_state=2)
+
+# print ("\ntest", test_set.head)
+
+
+
+
+corr_matrix = rawdata.corr()
+corr_matrix["Step"].sort_values(ascending=False)
+from pandas.plotting import scatter_matrix
+
+attributes = ["X", "Y", "Z","Step"]
+scatter_matrix(rawdata[attributes], figsize=(12, 8))
+save_fig("scatter_matrix_plot")
+
+rawdata.plot(kind="scatter", x="Step", y="X", alpha=0.1)
+plt.axis([0, 13, 0, 10])
+save_fig("X_vs_Step")
+
+rawdata.plot(kind="scatter", x="Step", y="Y", alpha=0.1)
+plt.axis([0, 13, 0, 6])
+save_fig("Y_vs_Step")
+
+rawdata.plot(kind="scatter", x="Step", y="Z", alpha=0.1)
+plt.axis([0, 13, 0, 2])
+save_fig("Z_vs_Step")
+
+X = rawdata[['X', 'Y', 'Z']]
+y = rawdata['Step']
+
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 
-train_set, test_set = train_test_split(rawdata, test_size=0.2, random_state=2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify = y)
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_classifier.fit(X_train, y_train)
+y_pred = rf_classifier.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
 
-print ("\ntest", test_set.head)
-
-
+print(f"Accuracy: {accuracy}")
+print("Classification Report:")
+print(report)
 
 
 
