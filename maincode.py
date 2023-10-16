931ea27242659e08cadfd70b91b3efcee5d47009
Saket-Bharade <147977902+Saket-Bharade@users.noreply.github.com>
2023-10-16 18:10:47 -0400

    finished code

--- maincode.py
+++ maincode.py
@@ -1,22 +1,29 @@
+
+#all the import stuff 
+
 import sys
 assert sys.version_info >= (3, 5)
-
-# Scikit-Learn ≥0.20 is required
 import sklearn
 assert sklearn.__version__ >= "0.20"
 import joblib
-# Common imports
 import os
-
-# To plot pretty figures
-
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import tarfile
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import seaborn as sns
+from sklearn.neighbors import KNeighborsClassifier
+
+#making graphs to understand the kind of data we are dealing with
+
 mpl.rc('axes', labelsize=14)
 mpl.rc('xtick', labelsize=12)
 mpl.rc('ytick', labelsize=12)
 
-# Where to save the figures
 PROJECT_ROOT_DIR = "."
 CHAPTER_ID = "histogram_plots"
 IMAGES_PATH = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID)
@@ -28,33 +35,27 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
     if tight_layout:
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
-    
 
+csv_file_path = "Project 1 Data.csv"  
+zip_file_path = "C:/Users/saket/Documents/GitHub/AER_850_project1.zip"
 
-import os
-import tarfile
-import pandas as pd
-
-# Define the paths and filenames
-csv_file_path = "Project 1 Data.csv"  # Provide the path to your local CSV file
-zip_file_path = "C:/Users/saket/Documents/GitHub/AER_850_project1.zip"  # Provide the desired output ZIP file path
-
-# Step 1: Convert the CSV file to a ZIP file
 with tarfile.open(zip_file_path, "w:gz") as zipf:
     zipf.add(csv_file_path, arcname=os.path.basename(csv_file_path))
 
-# Step 2: Open the ZIP file with pandas
+
 with tarfile.open(zip_file_path, "r:gz") as zipf:
-    # Find the name of the CSV file within the ZIP file
+ 
     csv_filename = os.path.basename(csv_file_path)
 
-    # Extract the CSV file from the ZIP file
+ 
     zipf.extract(csv_filename)
 
-# Step 3: Read the CSV file using pandas
+
 df = pd.read_csv(csv_filename)
 rawdata = df
-# Now you can work with the DataFrame 'df' as needed
+
+#understand the data 
+
 print (rawdata.head())
 print (rawdata.info())
 
@@ -82,14 +83,9 @@ X = rawdata[['X', 'Y', 'Z']]
 y = rawdata['Step']
 
 
+# machine learning happens here :o
 
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-import seaborn as sns
-from sklearn.neighbors import KNeighborsClassifier
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify = y)
 X_train = pd.DataFrame(X_train, columns=['X', 'Y', 'Z'])
@@ -147,6 +143,9 @@ plt.ylabel('Actual')
 plt.title('Confusion Matrix')
 plt.show()
 
+#we see that CVM has the best accuracy from the results so we will only use CVM
+
+
 joblib.dump(svm_classifier, 'svm_model.pkl')
 
 def predict_step(model_path, coordinates):
@@ -166,11 +165,13 @@ coordinates_to_predict = [
     [0,3.0625,1.93],
     [9.4,3,1.8],
     [9.4,3,1.3],
-    # Add more sets of coordinates as needed
+   
 ]
+
+#print predictions
 
 predicted_steps = predict_steps(model_path, coordinates_to_predict)
 
 for i, predicted_step in enumerate(predicted_steps):
-    print(f"Predicted Step {i + 1}: {predicted_step}")
+    print(f"\nPredicted Step {i + 1}: {predicted_step}")
 
